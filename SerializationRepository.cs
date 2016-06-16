581c6193f6a24547a4b1b2914aef7328cba23398
Alex Griciuc <agriciuc@gmail.com>
2016-06-16 23:21:57 +0300

    Dynamic objects and deserializing to special types

--- SerializationRepository.cs
+++ SerializationRepository.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Dynamic;
+using System.Collections.Generic;
 using JsonNetCore.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -67,6 +69,61 @@ namespace JsonNetCore
             );
 
             Console.WriteLine(personObjectSerialized);
+            Console.ReadLine();
+        }
+
+        public static void DynamicObjectsDemo() {
+            Console.Clear();
+            Console.WriteLine("***  Dynamic and ExpandoObject Demo ***");
+
+            Console.WriteLine("- Serialize");
+            // We use System.Dynamic.ExpandoObject class to create our dynamic object
+            dynamic authorDynamic = new ExpandoObject();
+            authorDynamic.FriendlyName = "Alex Griciuc";
+            authorDynamic.Likes = new List<string>() { "JSON.Net", ".NET Core", "Learning" };
+            authorDynamic.Happy = true;
+            string jsonDynamicAuthor = JsonConvert.SerializeObject(authorDynamic, Formatting.Indented);
+            Console.WriteLine(jsonDynamicAuthor);
+
+            Console.WriteLine("- Deserialize");
+            // Notice how we don't have to pass an object type anymore.
+            // We just pass the json string and deserialize it to a dynamic object
+            dynamic authorDeserialized = JsonConvert.DeserializeObject(jsonDynamicAuthor);
+            Console.WriteLine("Friendly Name: " + authorDeserialized.FriendlyName);
+            Console.ReadLine();
+        }
+
+        public static void DeserializeToSpecialTypes() {
+            Console.Clear();
+            Console.WriteLine("*** Types Demo ***");
+            string authorProperties = @"{
+                                   'name': 'Alex Griciuc',
+                                   'happy': true,
+                                   'favorites': 4
+                                 }
+                                ";
+
+            Console.WriteLine("- var");
+            var authorVar = JsonConvert.DeserializeObject(authorProperties);
+            Console.WriteLine(authorVar);
+
+            Console.WriteLine("- Dictionary");
+            Dictionary<string, string> authorDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(authorProperties);
+            Console.WriteLine(authorDictionary["name"]);
+
+            Console.WriteLine("- Anonymous types");
+            var authorAnonymousTypeDefinition = new
+            {
+                Name = string.Empty,
+                Happy = false,
+                Favorites = 0,
+                SomeOtherProp = string.Empty
+            };
+
+            // Notice how we use DeserializeAnonymousType instead of DeserializeObject
+            // to perform deserialization to an anonymous type
+            var authorAnonymous = JsonConvert.DeserializeAnonymousType(authorProperties, authorAnonymousTypeDefinition);
+            Console.WriteLine(authorAnonymous.Name);
             Console.ReadLine();
         }
     }
